4b519ec61ccf87dce35971026df7b1256035f7fe
CedCommerce <cedcommerce@gmail.com>
2015-09-23 18:13:00 +0530

    first commit

---
+++ YourPaymentModule/Model/PaymentMethodName.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace YourCompanyName\YourPaymentModule\Model;
+
+
+
+/**
+ * Pay In Store payment method model
+ */
+class PaymentMethodName extends \Magento\Payment\Model\Method\AbstractMethod
+{
+
+    /**
+     * Payment code
+     *
+     * @var string
+     */
+    protected $_code = 'paymentmethodname';
+
+    /**
+     * Availability option
+     *
+     * @var bool
+     */
+    protected $_isOffline = true;
+
+
+  
+
+}
